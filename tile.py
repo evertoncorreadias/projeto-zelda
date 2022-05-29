737121ecf78f08208de290564f650a0210caca7d
everton correa dias <93101117+evertoncorreadias@users.noreply.github.com>
2022-05-29 14:15:47 -0300

    criaçao de mapa

--- tile.py
+++ tile.py
@@ -4,5 +4,5 @@ from config import*
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.imagem = pygame.image.load('graphics/test/rock.png').convert_alpha() # CARREGANDO IMAGEM
-        self.rect = self.imagem.get_rect(topleft =pos)  # CRIANDO E POSICIONANDO A IMAGEM 
+        self.image = pygame.image.load('graphics/test/rock.png').convert_alpha() # CARREGANDO IMAGEM
+        self.rect = self.image.get_rect(topleft =pos)  # CRIANDO E POSICIONANDO A IMAGEM 
