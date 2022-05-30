cdb1e1a2c2ca5df226f2e390d277f5ce6c5b3fe2
everton correa dias <93101117+evertoncorreadias@users.noreply.github.com>
2022-05-30 20:50:34 -0300

    ADICIONADO COLISAO

--- jogador.py
+++ jogador.py
@@ -2,13 +2,15 @@ import pygame
 from config import*
 
 class Jogador(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups,sprites_obstaculos):
         super().__init__(groups)
         self.image = pygame.image.load('graphics/test/player.png').convert_alpha() # CARREGANDO IMAGEM
         self.rect = self.image.get_rect(topleft =pos)  # CRIANDO E POSICIONANDO A IMAGEM 
         
         self.direcao = pygame.math.Vector2()
         self.speed = 5
+        
+        self.sprites_obstaculos = sprites_obstaculos
         
     def controle(self):
         teclas = pygame.key.get_pressed()
@@ -28,9 +30,33 @@ class Jogador(pygame.sprite.Sprite):
             self.direcao.x = 0
             
     def mover(self,speed):
-        if self.direcao.magnitude() != 0:             # reduzir velocidade na diagonal
-            self.direcao = self.direcao.normalize()   # reduzir velocidade na diagonal
-        self.rect.center += self.direcao * speed
+        if self.direcao.magnitude() != 0:             # REDUZIR VELOCIDAD NA DIAGONAL
+            self.direcao = self.direcao.normalize()   # REDUZIR VELOCIDAD NA DIAGONAL
+        
+        
+        self.rect.x += self.direcao.x * speed       
+        self.colisao('horizontal')                  # COLISAO HORIZONTAL
+        self.rect.y += self.direcao.y * speed
+        self.colisao('vertical')                    # COLISAO VERTICAL
+        
+    def colisao(self,direcao):
+        if direcao == 'horizontal':
+            for sprite in self.sprites_obstaculos:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direcao.x > 0: # MOVENDO PARA DIREITA
+                        self.rect.right = sprite.rect.left  # COLISAO
+                        print("bateu")
+                    if self.direcao.x < 0: # MOVENDO PARA ESQUERDA
+                        self.rect.left = sprite.rect.right # COLISAO
+                        
+        if direcao == 'vertical':
+            for sprite in self.sprites_obstaculos:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direcao.y > 0: # MOVENDO PARA BAIXO
+                        self.rect.bottom = sprite.rect.top  # COLISAO
+                    if self.direcao.y < 0: # MOVENDO PARA CIMA
+                        self.rect.top = sprite.rect.bottom # COLISAO
+                    
             
     def update(self):
         self.controle()
